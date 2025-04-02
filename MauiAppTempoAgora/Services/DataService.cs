27d7ea257c18bbbf34b47450182dbb74e7d28803
EDUARDO SILVA <eduardo.silva1305@etec.sp.gov.br>
2025-04-02 00:27:58 -0300

    add

--- MauiAppTempoAgora/Services/DataService.cs
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -13,11 +13,11 @@ namespace MauiAppTempoAgora.Services
 
             string url = $"https://api.openweathermap.org/data/2.5/weather?" +
                 $"q={Cidade}&units=metric&appid={chave}";
-
-            using (HttpClient client = new HttpClient()) 
+            using (HttpClient client = new HttpClient())
             {
                 HttpResponseMessage resp = await client.GetAsync(url);
-                if (resp.IsSuccessStatusCode) 
+
+                if (resp.IsSuccessStatusCode)
                 {
                     string json = await resp.Content.ReadAsStringAsync();
 
@@ -25,28 +25,23 @@ namespace MauiAppTempoAgora.Services
 
                     DateTime time = new();
                     DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
-                    DateTime sunste = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
-
-
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
 
                     t = new()
                     {
                         lat = (double)rascunho["coord"]["lat"],
                         lon = (double)rascunho["coord"]["lon"],
                         description = (string)rascunho["weather"][0]["description"],
-                        main = (string)rascunho["weather"]["main"],
-                        temp_min = (double)rascunho["main"]["tem_min"],
-                        temp_max = (double)rascunho["main"]["tem_max"],
-                        speed = (double)rascunho["main"]["speed"],
-                        visibility = (int)rascunho["main"]["visibility"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"],
                         sunrise = sunrise.ToString(),
-                        sunset = sunrise.ToString(),
-                        
-
-                    };//fecha objeto do tempo 
-                }//fecha if se o estatus do servidor 
-            }//fecha laço
-                     
+                        sunset = sunset.ToString(),
+                    }; // Fecha obj do Tempo.
+                } // Fecha if se o status do servidor foi de sucesso
+            } // fecha laço using
 
             return t;
         }
